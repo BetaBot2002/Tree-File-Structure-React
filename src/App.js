f45a212b0dd5dd499c5297cc100a6757efbe5700
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2023-03-12 00:38:28 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
+const files={
+  name:"main",
+  children:[
+    {
+      name:"public",
+      children:[
+        {
+          name:"index.html"
+        },
+        {
+          name:"images",
+          children:[
+            {
+              name:"Image1.jpg"
+            },
+            {
+              name:"Image2.png"
+            }
+          ]
+        },
+        {
+          name:"videos",
+          children:[
+            {
+              name:"video1.mkv"
+            },
+            {
+              name:"video2.mp4"
+            }
+          ]
+        },
+        {
+          name:"output.txt"
+        }
+      ]
+    },
+    {
+      name:"src",
+      children:[
+        {
+          name:"App.css"
+        },
+        {
+          name:"App.js"
+        }
+      ]
+    },
+    {
+      name:"package.json"
+    }
+  ]
+}
+
+function Entry({entry,depth}){
+const [isExpanded,setExpanded]=useState(false);
+
+
+  return <div>
+    <div onClick={()=> setExpanded(!isExpanded)} style={entry.children && {cursor:`pointer`}}>
+      {entry.children && (isExpanded?"📂":"📁")}
+      {!entry.children && "📄"}
+      {entry.name}
+    </div>
+    {isExpanded &&(
+      <div style={{paddingLeft:`${depth*10}px`}}>
+        {entry.children?.map((entry)=>(
+          <Entry entry={entry} depth={depth+1}/>
+      ))}
+      </div>
+    )}
+  </div>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {files.children.map((entry)=>(
+        <Entry entry={entry} depth={1}/>
+      ))}
     </div>
   );
 }
